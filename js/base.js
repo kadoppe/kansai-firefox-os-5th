dffad73f3f21650847b66fb4c1b1fc745a539f29
Kohei Kadowaki <kadowaki@share-wis.com>
2013-11-23 10:45:06 +0900

    Remove cosole log

--- js/base.js
+++ js/base.js
@@ -2,7 +2,6 @@ if (navigator.mozApps) {
     var checkIfInstalled = navigator.mozApps.getSelf();
     checkIfInstalled.onsuccess = function () {
         if (checkIfInstalled.result) {
-            console.log('aaaa')
             // Already installed
             var installationInstructions = document.querySelector("#installation-instructions");
             if (installationInstructions) {
@@ -10,7 +9,6 @@ if (navigator.mozApps) {
             }
         }
         else {
-            console.log('aaaa')
             var install = document.querySelector("#install"),
                 manifestURL = location.href.substring(0, location.href.lastIndexOf("/")) + "/manifest.webapp";
             install.className = "show-install";
